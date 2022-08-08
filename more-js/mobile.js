9348ac9cacae8decd4a5b595a9272862f6c0dd79
zahid4523114 <dewanmohammodzahidhasan@gmail.com>
2022-08-08 04:46:54 -0600

    lowest

--- more-js/mobile.js
+++ more-js/mobile.js
@@ -1,42 +1,56 @@
 let phones = [
   {
-    name: "iphone",
+    name: 'iphone',
     price: 146999.0,
-    Released: "2020, October 23",
-    OS: "iOS 14.1, up to iOS 14.2",
+    Released: '2020, October 23',
+    OS: 'iOS 14.1, up to iOS 14.2',
     Display: '6.1" 1170x2532 pixels',
-    Camera: "12MP 2160p",
-    RAM: " 6GB RAM Apple A14 Bionic",
-    Battery: "2815mAh Li-Ion",
+    Camera: '12MP 2160p',
+    RAM: ' 6GB RAM Apple A14 Bionic',
+    Battery: '2815mAh Li-Ion',
   },
   {
-    name: "samsung",
+    name: 'samsung',
     price: 35000,
-    Released: "2020, October 23",
-    OS: "iOS 14.1, up to iOS 14.2",
+    Released: '2020, October 23',
+    OS: 'exynos',
     Display: '6.1" 1170x2532 pixels',
-    Camera: "12MP 2160p",
-    RAM: " 6GB RAM Apple A14 Bionic",
-    Battery: "2815mAh Li-Ion",
+    Camera: '12MP 2160p',
+    RAM: ' 6GB RAM ',
+    Battery: '2815mAh Li-Ion',
   },
   {
-    name: "xiaoumi",
+    name: 'xiaoumi',
     price: 20000,
-    Released: "2020, October 23",
-    OS: "iOS 14.1, up to iOS 14.2",
+    Released: '2020, October 23',
+    OS: 'iOS 14.1, up to iOS 14.2',
     Display: '6.1" 1170x2532 pixels',
-    Camera: "12MP 2160p",
-    RAM: " 6GB RAM Apple A14 Bionic",
-    Battery: "2815mAh Li-Ion",
+    Camera: '12MP 2160p',
+    RAM: ' 6GB RAM Apple A14 Bionic',
+    Battery: '2815mAh Li-Ion',
   },
   {
-    name: "vivo",
+    name: 'vivo',
     price: 14000,
-    Released: "2020, October 23",
-    OS: "iOS 14.1, up to iOS 14.2",
+    Released: '2020, October 23',
+    OS: 'iOS 14.1, up to iOS 14.2',
     Display: '6.1" 1170x2532 pixels',
-    Camera: "12MP 2160p",
-    RAM: " 6GB RAM Apple A14 Bionic",
-    Battery: "2815mAh Li-Ion",
+    Camera: '12MP 2160p',
+    RAM: ' 6GB RAM Apple A14 Bionic',
+    Battery: '2815mAh Li-Ion',
   },
 ];
+
+function checkLowest(phone) {
+  let checkPhone = [];
+  for (let i = 0; i < phone.length; i++) {
+    let indexOfPhone = phone[i];
+    if (indexOfPhone.price < phone[0].price) {
+      checkPhone = indexOfPhone;
+    }
+  }
+  return checkPhone;
+}
+let getPhone = checkLowest(phones);
+
+console.log(getPhone);
